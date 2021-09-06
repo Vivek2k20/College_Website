b2c217f9138bed7fa2522caa4a184f4315afd5ba
Vivek M R <vivekmrofficial@gmail.com>
2021-09-06 17:54:39 +0530

    Designed Departments Page

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 '''
 #################################################################
-Importing stuff and initializing
+Importing stuff and initializing app
 #################################################################
 '''
 from flask import Flask,render_template,request,redirect,jsonify
@@ -14,13 +14,38 @@ app = Flask(__name__,template_folder = 'templates',static_folder='static')
 
 '''
 #################################################################
-Home
+Home Page
 #################################################################
 '''
-@app.route("/",methods=["GET","POST"])
+@app.route("/",methods=["GET"])
 def index():
     return render_template('home.html')
 
 
+
+
+
+
+'''
+#################################################################
+Courses Page
+#################################################################
+'''
+@app.route("/departments",methods=["GET"])
+def departments():
+    return render_template('departments.html')
+
+
+
+
+
+
+
+
+'''
+##################################################################
+Launching App
+##################################################################
+'''
 if __name__ == '__main__':
     app.run(debug=True)
